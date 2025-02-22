147303109e8f88f1b515c6180df4712dfaf503dc
Eric Ramirez <ericiramirez25@gmail.com>
2025-02-21 19:22:53 -0800

    Editing home page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Hero />} />
+        <Route path="/" element={<Navigate to="Website" replace />} />
+        <Route path="/Website" element={<Hero />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
